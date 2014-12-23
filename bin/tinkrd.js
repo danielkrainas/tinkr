34879918b39fa0e04026d72a29adea72f1fc0002
dkrainas87@gmail.com <dkrainas87@gmail.com>
2014-12-22 21:09:31 -0500

    allow passing of options from cli

--- bin/tinkrd.js
+++ bin/tinkrd.js
@@ -1,5 +1,21 @@
 #!/usr/bin/env node
-
 var path = require('path');
+var program = require('commander');
+var pkg = require(path.join(__dirname, '../package.json'));
 
-require(path.join(__dirname, '../server'));
+program
+    .version(pkg.version)
+    .option('--home [path]', 'The directory to store tinkr related data.')
+    .option('--port [port]', 'The port the daemon should listen on.');
+
+program.parse(process.argv);
+if (program.home) {
+    process.env.TINKR_HOME = program.home;
+}
+
+if (program.port) {
+    process.env.TINKR_PORT = program.port;
+}
+
+var tinkr = require(path.join(__dirname, '../server'));
+tinkr.startServer();
--- server.js
+++ server.js
@@ -13,21 +13,28 @@ var config = require('./config');
 var app = express();
 require('./config/express')(app);
 
-async.series([
-    stubs.load,
-    snapshots.load,
-    projects.load,
-    projects.startAllAuto
-], function (err) {
-    if (err) {
-        console.error('could not start ' + config.app.name);
-        return console.error(err);
-    }
-    
-    var server = http.Server(app);
-    var port = config.port;
-    server.listen(port);
-    console.log(config.app.name + ' (' + process.env.NODE_ENV + ')' + ' started on port ' + port);
-});
+function startServer() {
+    async.series([
+        stubs.load,
+        snapshots.load,
+        projects.load,
+        projects.startAllAuto
+    ], function (err) {
+        if (err) {
+            console.error('could not start ' + config.app.name);
+            return console.error(err);
+        }
+        
+        var server = http.Server(app);
+        var port = config.port;
+        server.listen(port);
+        console.log(config.app.name + ' (' + process.env.NODE_ENV + ')' + ' started on port ' + port);
 
-exports = module.exports = app;
+    });
+}
+
+if (!module.parent) {
+    startServer();
+}
+
+exports.startServer = startServer;
